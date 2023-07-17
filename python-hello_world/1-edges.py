71f0528ba71062fa70fde5adaae12317f444ceee
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-07-17 16:00:16 +0300

    hanges

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,6 @@
+word_first_3 =input()
+word_last_2=input()
+middle_word =input()
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
