836ba1233a1095a3328a3c3bdfd3feed67cdda87
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-07-18 15:31:13 +0300

    new commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-word_first_3 = input()
-word_last_2 = input()
-middle_word = input()
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[-2:]
+middle_word = word[3:7]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
