67d9ae7d61ad074961fb9750ec79089e3bb9b098
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-07-23 13:48:03 +0300

    changes

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
 if number==0:
     print(f"{number} is zero")
 elif number <0:
--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,4 +1,6 @@
-
+#!/usr/bin/python3
+for num in range(99):
+    print("Decimal: {}, Hexadecimal: {}".format(num, hex(num)))
 
 
   
