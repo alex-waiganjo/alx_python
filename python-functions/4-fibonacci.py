768ca1cac2f63d1cb44dd6e699ac1ee4d8d2dc45
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-07-25 10:26:04 +0300

    Test it

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -4,6 +4,6 @@ def fibonacci_sequence(n):
     a, b = 0, 1
     while a < n:
         fib_list.append(a)
-        a, b = b, a + b
+        a, b = b, a + b        
     return fib_list
-print(fibonacci_sequence(0))
+# print(fibonacci_sequence(6))
