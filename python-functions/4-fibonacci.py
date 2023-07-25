72b9a387643fcb94a8be6c7d23cced8cdae8bfc4
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-07-25 10:32:38 +0300

    new changes

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,8 +2,8 @@
 def fibonacci_sequence(n):     
     fib_list = []
     a, b = 0, 1
-    while a < n:
+    while a < n or a<=n:
         fib_list.append(a)
         a, b = b, a + b        
     return fib_list
-# print(fibonacci_sequence(6))
+print(fibonacci_sequence(4181))
