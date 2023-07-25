d7710fc318aa782ca74afc65d8a014f961afdc4e
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-07-25 10:57:24 +0300

    Prime Numbers

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,8 +2,12 @@
 def fibonacci_sequence(n):     
     fib_list = []
     a, b = 0, 1
-    while a < n or a<=n:
+    if a<n:
         fib_list.append(a)
-        a, b = b, a + b        
+        a, b = b, a + b 
+        while a<=n:   
+         fib_list.append(a)
+         a, b = b, a + b        
+        return fib_list    
     return fib_list
 print(fibonacci_sequence(4181))
---
+++ python-functions/5-prime.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def is_prime(number):
+    if number%2==0:
+        return "True"
+    else:
+        return "False"
+print(is_prime(5))
