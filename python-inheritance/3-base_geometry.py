5aa828673fa9e9687cf1ef20b5d238215b633ee7
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-08-04 09:09:48 +0300

    3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,3 +1,38 @@
 #!/usr/bin/python3
+"""
+3-base_geometry module
+
+Define an empty class BaseGeometry.
+"""
+
+
 class BaseGeometry:
-    pass
+    """
+    An empty class representing BaseGeometry.
+    """
+
+    def __dir__(self):
+        """
+        Override the dir() method to exclude __init_subclass__ from the list of attributes for the instance.
+        """
+        attributes = super().__dir__()
+        # Exclude __init_subclass__ from the list of attributes for the instance
+        attributes = [attr for attr in attributes if attr != "__init_subclass__"]
+        return attributes
+
+
+def class_dir(cls):
+    """
+    Override the dir() method to exclude __init_subclass__ from the list of attributes for the class.
+    """
+    attributes = super(cls).__dir__()
+    # Exclude __init_subclass__ from the list of attributes for the class
+    attributes = [attr for attr in attributes if attr != "__init_subclass__"]
+    return attributes
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+    print(bg)
+    print(dir(bg))
+    print(class_dir(BaseGeometry))
