59294924932a208c12ff7518874cfc95f2db2da3
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-08-04 09:20:36 +0300

    u

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,10 +1,36 @@
 #!/usr/bin/python3
-"""Empty BaseGeometry class"""
+"""
+4-base_geometry module
+
+Define a class BaseGeometry with the area() method.
+"""
 
 
 class BaseGeometry:
-    """Class Geometry"""
+    """
+    BaseGeometry class.
+    """
 
     def area(self):
-        """Raises Exception only"""
-        raise Exception('area() is not implemented')
+        """
+        Public instance method to calculate area.
+        """
+        raise Exception("area() is not implemented")
+
+    def __dir__(self):
+        """
+        Override the dir() method to include the area() method in the list of attributes for the instance.
+        """
+        attributes = super().__dir__()
+        # Add area() to the list of attributes for the instance
+        attributes.append('area')
+        return attributes
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+    try:
+        print(bg.area())
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+    print(dir(bg))
