5a6a62f4731760b9dc386d3fd15096356e8b5f2b
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-08-23 16:46:40 +0300

    7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {}<mysql_username> <mysql_password> <database_name>"
               .format(
-            sys.argv[0]))
+                  sys.argv[0]))
         sys.exit(1)
 
     mysql_username = sys.argv[1]
