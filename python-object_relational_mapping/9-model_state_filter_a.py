f9a6cd21fb0112cdbdb5c0ad93485a855f7041a9
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-08-23 16:55:34 +0300

    orm

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,8 +11,9 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(
-            sys.argv[0]))
+        print("Usage: {}<mysql_username> <mysql_password> <database_name>"
+              .format(
+                  sys.argv[0]))
         sys.exit(1)
 
     mysql_username = sys.argv[1]
