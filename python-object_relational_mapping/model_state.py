247b55ca9de5a5572eef0f8c8cc931d1f1ab6dd3
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-08-23 12:51:42 +0300

    few changes

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,34 +1,15 @@
-#!/usr/bin/env python3
-"""Script to list all State objects containing the letter "a" from a MySQL database using SQLAlchemy"""
+#!/usr/bin/python3
+'''
+contains the class definition of a State and an
+instance Base = declarative_base()
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(
-            sys.argv[0]))
-        sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create an engine to connect to the MySQL server
-    engine_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        mysql_username, mysql_password, database_name)
-    engine = create_engine(engine_url)
-
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query and print State objects containing the letter "a" in ascending order by states.id
-    states_with_a = session.query(State).filter(
-        State.name.like('%a%')).order_by(State.id).all()
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
+class State(Base):
+    ''' Empty class that inherits from Base '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
