0e01e77afee692185474b9a4bb3eea6671d506a1
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-10-05 22:49:10 +0300

    r

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,69 +2,35 @@ import requests
 import sys
 
 
-id = sys.argv[1]
-user_data_url = f'https://jsonplaceholder.typicode.com/users/{id}'
-user_todos_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-output = 0
 
-# Fetch user data
-user_data = requests.get(user_data_url)
-user_todo = requests.get(user_todos_url)
+def  get_data(id):
+    user_data_url = f'https://jsonplaceholder.typicode.com/users/{id}'
+    user_todos_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    output = 0
 
-# Parse user data
-json_output = user_data.json()
-emp_name = json_output["name"]
-p = user_todo.json()
-total = len(p)
+    # Fetch  data
+    user_data = requests.get(user_data_url)
+    user_todo = requests.get(user_todos_url)
 
-# Display the titles of completed tasks
-m = []
-for task in user_todo.json():
-    if task["completed"]:
-        output += 1
-        m.append(task["title"])
+    # Parse data
+    json_output = user_data.json()
+    emp_name = json_output["name"]
+    json_user_todo = user_todo.json()
+    total_tasks = len(json_user_todo)
 
-
-# print(len(f'Employee {emp_name} is done with tasks ({output}/{total}):'))
-print('Employee {} is done with tasks({}/{}):'
-      .format(emp_name, output, total))
-for task_title in m:
-    print(f'\t{task_title}')
+    # Display the titles of completed tasks
+    m = []
+    for task in user_todo.json():
+        if task["completed"]:
+            output += 1
+            m.append(task["title"])
 
 
-# if __name__ == '__main__':
-#     if len(sys.argv) != 2:
-#         print("Usage: python3 script_name.py employee_id")
-#         sys.exit(1)
-
-#     id = int(sys.argv[1])
-#     get_data(id)
+    print(f'Employee {emp_name} is done with tasks ({output}/{total_tasks}):')
+    for task_title in m:
+        print(f'\t{task_title}')
 
 
-# def get_data(id):
-#     user_data_url = f'https://jsonplaceholder.typicode.com/users/{id}'
-#     user_todos_url= f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-#     output = 0
-#     user_data = requests.get(user_data_url)
-#     user_todo = requests.get(user_todos_url)
-#     json_output= user_data.json()
-#     emp_name = json_output["name"]
-
-
-#     user_todo.json()
-#     fr = user_todo.json()
-#     total_no =  len(fr)
-
-#     m = []
-
-
-#     fr = user_todo.json()
-#     for items in range(len(fr)):
-#         nose= fr[items]["title"]
-#         nos= fr[items]["completed"]
-#         if nos:
-#             output+=1
-#             m.append(nose)
-#             for v in m:
-#                 print(v)
-#     print(f'Employee {emp_name} is done with tasks ({output}/{total_no})')
+if __name__ =='__main__':
+    id = sys.argv[1]
+    get_data(id)
