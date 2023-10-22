97d82d4da52acf2adcdd225092f9bfb5d5d1eec2
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-10-22 19:33:33 +0300

    api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,38 +1,55 @@
-#!/usr/bin/python3
-"""
-Request from API; Return TODO list progress of all employees
-Export this data to JSON
-"""
-import json
-import requests
+import json, requests, sys
 
-
-def all_to_json():
-    """return API data"""
-    USERS = []
-    userss = requests.get("http://jsonplaceholder.typicode.com/users")
-    for u in userss.json():
-        USERS.append((u.get('id'), u.get('username')))
-    TASK_STATUS_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for t in todos.json():
-        TASK_STATUS_TITLE.append((t.get('userId'),
-                                  t.get('completed'),
-                                  t.get('title')))
-
-    """export to json"""
-    data = dict()
-    for u in USERS:
-        t = []
-        for task in TASK_STATUS_TITLE:
-            if task[0] == u[0]:
-                t.append({"task": task[2], "completed": task[1],
-                          "username": u[1]})
-        data[str(u[0])] = t
-    filename = "todo_all_employees.json"
-    with open(filename, "w") as f:
-        json.dump(data, f, sort_keys=True)
-
+def get_employee_data(employee_id):
+    # URL to fetch employee details for all employees
+    employee_url = f"https://jsonplaceholder.typicode.com/users"
+    
+    try:
+        # Fetch data for all employees
+        response_employee = requests.get(employee_url)
+        response_employee.raise_for_status()
+        employee_data = response_employee.json()
+        
+        all_data = {}
+        
+        # Iterate over all employees
+        for employee in employee_data:
+            employee_id = employee["id"]
+            employee_name = employee["name"]
+            
+            # Fetch to do list
+            _, todos_data = fetch_todos(employee_id)
+            
+            # Extract completed tasks for the employee
+            completed_tasks = [{"username": employee_name, "task": todo["title"], "completed": todo["completed"]} for todo in todos_data if todo["completed"]]
+            
+            all_data[employee_id] = completed_tasks
+            
+        return all_data
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+        
+def fetch_todos(employee_id):
+    # URL to fetch employee's to do list
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    
+    try:
+        response_todos = requests.get(todos_url)
+        response_todos.raise_for_status()
+        todos_data = response_todos.json()
+        
+        return todos_data
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+        
+def main():
+    all_data = get_employee_data()
+    
+    # Export data to json
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_data, json_file)
 
 if __name__ == "__main__":
-    all_to_json()
+    main()
